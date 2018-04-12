a9071ac5dd5230bc591b4d6657ad663de4334d87
harijs.me <harijs.melderis.auzenbergs@gmail.com>
2018-04-12 14:31:36 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,14 +2,19 @@
 import socket
 import time
 import random
+import datetime
+import os
 
-HOST = '192.168.1.2'    # The remote host
+HOST = '192.168.1.2'      # The remote host
 PORT = 50007              # The same port as used by the server
+BACKUP_DIR = './backup_temp_logs/'
+TIMEOUT = 15.0
 
 class Connect(object):
 
     def connect(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('')
         print('connecting to host')
         sock.connect((HOST, PORT))
         return sock
@@ -25,8 +30,18 @@ class Connect(object):
         while data:
             data = sock.recv(1024)
             recv_data += data
-            print('response: '+ recv_data)
-        return recv_data
+        return 'response: '+ recv_data
+    def write(self, string):
+        try:
+            if not os.path.exists(BACKUP_DIR):
+                os.makedirs(BACKUP_DIR)
+
+            file=open(BACKUP_DIR+datetime.datetime.now().strftime("%d.%m.%Y")+".txt",mode="a")
+            file.write(string+'\n')
+            file.close()
+            return 'OK!'
+        except:
+            return 'ERROR:'
 
 
 connect = Connect()
@@ -34,5 +49,12 @@ starttime=time.time()
 
 while True:
   send_text = str(random.random())
-  connect.send(send_text)
-  time.sleep(5.0 - ((time.time() - starttime) % 5.0))
+  response = connect.send(send_text)
+  if "error logging data" in response:
+    status = connect.write(send_text)
+    print response
+    print (status+ " writing backup log on SD: "+ send_text)
+  else:
+    print response
+  
+  time.sleep(TIMEOUT - ((time.time() - starttime) % TIMEOUT))
