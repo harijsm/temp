c4a5cb06a5d92e2b421a7935b3a6cf06f3571d04
harijs.me <harijs.melderis.auzenbergs@gmail.com>
2018-04-12 16:10:15 +0300

    Update client.py
    try to read temp from DS18B20

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
 # Echo client program
 import socket
 import time
-import random
 import datetime
 import os
+import glob
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
 HOST = '192.168.1.2'      # The remote host
 PORT = 50007              # The same port as used by the server
 BACKUP_DIR = './backup_temp_logs/'
 TIMEOUT = 15.0
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')
+
+if len(device_folder) > 0:
+    device_file = device_folder[0] + '/w1_slave'
+else:
+    device_file = False
 
 class Connect(object):
 
@@ -46,13 +57,43 @@ class Connect(object):
         except:
             return 'ERROR:'
 
+def read_temp_raw():
+    if device_file == False:
+        return False
+
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    if lines == False:
+        return False
+
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp = float(temp_string) / 1000.0
+        return temp
 
 connect = Connect()
 starttime=time.time()
 
 while True:
-    send_text = str(random.random())
-    response = connect.send(send_text)
+    try:
+        send_text = read_temp()
+        if send_text != False:
+            response = connect.send(send_text)
+        else:
+            response = "error reading temp from DS18B20"
+    except:
+        response = "error reading temp from DS18B20"
+    
     if "error logging data" in response:
         status = connect.write(send_text)
         print response
